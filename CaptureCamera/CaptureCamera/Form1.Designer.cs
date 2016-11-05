7b240f3579a0722ccb409c159e4f4f574adf29e0
amacar <amadej.pevec@gmail.com>
2016-11-05 21:23:11 +0100

    First commit
    First commit of an app

---
+++ CaptureCamera/CaptureCamera/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CaptureCamera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.capture = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(4, 489);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(164, 28);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // capture
+            // 
+            this.capture.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.capture.BackgroundImage = global::CaptureCamera.Properties.Resources.kamera1;
+            this.capture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.capture.FlatAppearance.BorderSize = 0;
+            this.capture.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DeepSkyBlue;
+            this.capture.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.capture.Location = new System.Drawing.Point(174, 488);
+            this.capture.Name = "capture";
+            this.capture.Size = new System.Drawing.Size(470, 64);
+            this.capture.TabIndex = 3;
+            this.capture.UseVisualStyleBackColor = false;
+            this.capture.Click += new System.EventHandler(this.capture_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 558);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.capture);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CaptureCamera";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button capture;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ CaptureCamera/CaptureCamera/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace CaptureCamera
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        }
+
+        private VideoCaptureDevice videoSource;
+        private Bitmap frameImage;
+        private FilterInfoCollection videosources;
+        private bool captureCanceled = true;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videosources = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videosources != null)
+            {
+                if (videosources.Count > 0)
+                {
+                    foreach (FilterInfo VideoCaptureDevice in videosources)
+                        comboBox1.Items.Add(VideoCaptureDevice.Name);
+
+                    if (videosources.Count == 1)
+                        comboBox1.SelectedIndex = 0;
+                }
+                else
+                {
+                    string text = "No camera detected!";
+                    RectangleF rectf = new RectangleF(pictureBox1.Width / 2 - 130, pictureBox1.Height / 2, 300, 200);
+                    frameImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    Graphics g = Graphics.FromImage(frameImage);
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                    g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                    g.DrawString(text, new Font("Tahoma", 18), Brushes.Red, rectf);
+                    g.Flush();
+
+                    pictureBox1.Image = frameImage;
+                }
+            }
+
+        }
+
+        void videoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            if (frameImage != null)
+                frameImage.Dispose();
+            frameImage = null;
+            try
+            {
+                frameImage = new Bitmap(eventArgs.Frame);
+            }
+            catch (Exception ex) { }
+
+            if (pictureBox1.Image != null)
+            {
+                try
+                {
+                    this.BeginInvoke(new MethodInvoker(delegate () { pictureBox1.Image.Dispose(); pictureBox1.Image = frameImage; }));
+                }
+                catch (Exception ex) { }
+            }
+
+        }
+
+        private void capture_Click(object sender, EventArgs e)
+        {
+            if (frameImage != null)
+            {
+                try
+                {
+                    string path = getSavePath();
+                    if (path != null)
+                    {
+                        frameImage.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        captureCanceled = false;
+                        Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    RectangleF rectf = new RectangleF(pictureBox1.Width / 2 - 130, pictureBox1.Height / 2, 300, 200);
+                    frameImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    Graphics g = Graphics.FromImage(frameImage);
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                    g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+                    g.DrawString("Error when saving captured frame!", new Font("Tahoma", 18), Brushes.Red, rectf);
+                    g.Flush();
+
+                    pictureBox1.Image = frameImage;
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseVideoSource();
+            if (captureCanceled && saveOnCaptureCanceled())
+            {
+                try
+                {
+                    string path = getSavePath();
+                    if (path != null)
+                    {
+                        var image = new Bitmap(Properties.Resources.nophoto);
+                        image.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    }
+                }
+                catch (Exception ex) { }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex < 0)
+                return;
+
+            CloseVideoSource();
+            string text = "Initializing camera...";
+            videoSource = new VideoCaptureDevice(videosources[comboBox1.SelectedIndex].MonikerString);
+
+            try
+            {
+                if (videoSource.VideoCapabilities.Length > 0)
+                {
+                    string highestSolution = "0;0";
+                    for (int i = 0; i < videoSource.VideoCapabilities.Length; i++)
+                        if (videoSource.VideoCapabilities[i].FrameSize.Width > Convert.ToInt32(highestSolution.Split(';')[0]))
+                            highestSolution = videoSource.VideoCapabilities[i].FrameSize.Width.ToString() + ";" + i.ToString();
+                    //videoSource.VideoResolution = videoSource.VideoCapabilities[Convert.ToInt32(highestSolution.Split(';')[1])];
+                }
+            }
+            catch { }
+
+            videoSource.NewFrame += new AForge.Video.NewFrameEventHandler(videoSource_NewFrame);
+
+            RectangleF rectf = new RectangleF(pictureBox1.Width / 2 - 130, pictureBox1.Height / 2, 300, 200);
+            frameImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(frameImage);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            g.DrawString(text, new Font("Tahoma", 18), Brushes.Red, rectf);
+            g.Flush();
+
+            pictureBox1.Image = frameImage;
+
+            videoSource.Start();
+        }
+
+        private void CloseVideoSource()
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.NewFrame -= new NewFrameEventHandler(videoSource_NewFrame);
+                videoSource = null;
+
+            }
+        }
+
+        private string getSavePath()
+        {
+            try
+            {
+                string[] props = File.ReadAllLines("setup.ini");
+                foreach (string prop in props)
+                {
+                    if (prop.StartsWith("path"))
+                    {
+                        string[] propSplitted = prop.Split('=');
+                        return propSplitted[1].Trim();
+                    }
+                }
+            }
+            catch (Exception ex) { }
+
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.FileName = "capture.jpg";
+            savefile.Filter = "Jpg files (*.jpg)|*.jpg";
+            if (savefile.ShowDialog() == DialogResult.OK)
+                return savefile.FileName;
+
+            return null;
+        }
+
+        private bool saveOnCaptureCanceled()
+        {
+            try
+            {
+                string[] props = File.ReadAllLines("setup.ini");
+                foreach (string prop in props)
+                {
+                    if (prop.StartsWith("saveOnCancel"))
+                    {
+                        string[] propSplitted = prop.Split('=');
+                        return propSplitted[1].Trim().ToLower() == "true";
+                    }
+                }
+            }
+            catch (Exception ex) { }
+
+            return false;
+        }
+    }
+}
